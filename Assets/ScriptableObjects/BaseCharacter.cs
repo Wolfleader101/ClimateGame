91a6992679b1cb6ec805aa9f59c68f8338ae2068
Wolfleader <james.morgan@iinet.net.au>
2023-03-17 11:40:44 +0800

    init

---
+++ Assets/ScriptableObjects/BaseCharacter.cs
@@ -0,0 +1,47 @@
+﻿using Sirenix.OdinInspector;
+using Sirenix.Serialization;
+using UnityEngine;
+
+namespace ScriptableObjects.Characters
+{
+    [CreateAssetMenu(fileName = "Character", menuName = "Character")]
+    public class BaseCharacter : ScriptableObject
+    {
+        [HorizontalGroup("Split", .5f, Title = "Stats")] 
+        
+        [SerializeField, BoxGroup("Split/Health"), LabelWidth(100f)]
+        private float startingHealth = 100f;
+        
+        [SerializeField, BoxGroup("Split/Health"), LabelWidth(100f)]
+        private float maxHealth = 100f;
+
+        [SerializeField, BoxGroup("Split/Stamina"), LabelWidth(100f)]
+        private float startingStamina = 100f;
+        
+        [SerializeField, BoxGroup("Split/Stamina"), LabelWidth(100f)]
+        private float maxStamina = 100f;
+        
+        
+        [HorizontalGroup("Movement", 1f, Title = "Movement")]
+        [SerializeField, BoxGroup("Movement/1", false)]
+        private float movementSpeed = 10f;
+        
+        [SerializeField, BoxGroup("Movement/1")]
+        private float jumpHeight = 2f;
+        
+        [SerializeField, BoxGroup("Movement/1")]
+        private bool canDoubleJump = true;
+        
+        [SerializeField, BoxGroup("Movement/1")]
+        private float gravityScaleMultiplier = 1.5f;
+        
+        public float StartingHealth => startingHealth;
+        public float MaxHealth => maxHealth;
+        public float StartingStamina => startingStamina;
+        public float MaxStamina => maxStamina;
+        public float MovementSpeed => movementSpeed;
+        public float JumpHeight => jumpHeight;
+        public float GravityScaleMultiplier => gravityScaleMultiplier;
+        public bool CanDoubleJump => canDoubleJump;
+    }
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,105 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using ScriptableObjects.Characters;
+using UnityEngine;
+using UnityEngine.InputSystem;
+using UnityEngine.InputSystem.Interactions;
+
+[RequireComponent(typeof(PlayerInput), typeof(CharacterController))]
+public class PlayerController : MonoBehaviour
+{
+    [SerializeField] private bool useVr;
+
+    [SerializeField] private float mouseSens = 10f;
+    
+    [SerializeField] private Transform cameraTransform;
+    [SerializeField] private CharacterController characterController;
+    [SerializeField] private float gravityScale = -9.81f;
+    [SerializeField] private BaseCharacter character;
+
+    // the shooting accuracy (of the player) is a value between 0 and 1.
+    // factors like moving, jumping, crouching, ADS will all improve said accuracy
+    // Weapon's also have their own max accuracy which is determined by the type of gun and its attachments
+    private float _shootingAccuracy = 0.75f;
+    public event Action<bool, bool, float> OnShootEvent;
+    
+    private float _mouseX;
+    private float _mouseY;
+    private float _xRotation = 0f;
+    private float _yRotation = 0f;
+
+    private float _xPos;
+    private float _zPos;
+
+
+    private Vector3 _velocity;
+    
+    private void Start()
+    {
+        if (characterController == null) characterController = GetComponent<CharacterController>();
+
+        //lock cursor to mid
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (characterController.isGrounded && _velocity.y < 0)
+        {
+            _velocity.y = -2f;
+        }
+
+        _xRotation -= _mouseY;
+        _yRotation -= -_mouseX;
+        _xRotation = Mathf.Clamp(_xRotation, -90f, 90f);
+
+        // fix rotation LMAO
+        // ez fix
+        // just rotate torso and head NOT whole game object
+        transform.localRotation = Quaternion.Euler(_xRotation, _yRotation, 0f);
+
+        Vector3 motion = transform.right * _xPos + transform.forward * _zPos;
+        characterController.Move(motion * (character.MovementSpeed * Time.deltaTime));
+
+        _velocity.y += (character.GravityScaleMultiplier * gravityScale) * Time.deltaTime;
+        characterController.Move(_velocity * Time.deltaTime);
+    }
+    
+    public void OnMove(InputAction.CallbackContext value)
+    {
+        Vector2 direction = value.ReadValue<Vector2>();
+        _xPos = direction.x;
+        _zPos = direction.y;
+    }
+
+    public void OnLook(InputAction.CallbackContext value)
+    {
+        Vector2 direction = value.ReadValue<Vector2>();
+        _mouseX = direction.x * mouseSens * Time.deltaTime;
+        _mouseY = direction.y * mouseSens * Time.deltaTime;
+    }
+
+    public void OnJump(InputAction.CallbackContext value)
+    {
+        if (character.CanDoubleJump && value.performed && value.interaction is MultiTapInteraction)
+        {
+            _velocity.y = Mathf.Sqrt(character.JumpHeight * -2f * gravityScale);
+            Debug.Log("Double Jump");
+        }
+
+        if (characterController.isGrounded && value.started)
+        {
+            _velocity.y = Mathf.Sqrt(character.JumpHeight * -2f * gravityScale);
+        }
+    }
+
+    public void OnShoot(InputAction.CallbackContext context)
+    {
+        bool holdingShoot = context.interaction is HoldInteraction && context.performed;
+        bool shooting = context.started || context.performed;
+
+        OnShootEvent?.Invoke(shooting, holdingShoot, _shootingAccuracy);
+    }
+}
