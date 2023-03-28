e65b412e8676f11ef673e39f7947b501aec382f9
Blake C <blake.caldwell5@gmail.com>
2023-03-28 15:35:22 +0800

    Scene setup

---
+++ Assets/Scripts/Airconditioner/Aircon.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class Screen : MonoBehaviour
+{
+    
+    public TextMeshProUGUI text;
+
+    private int value;
+
+    private int targetvalue;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
