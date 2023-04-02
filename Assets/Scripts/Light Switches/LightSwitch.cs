d998baf229bec296317b0c676cb6ab9f72a40cfd
Darcy Wilson <26544407+VoxAndrews@users.noreply.github.com>
2023-04-02 22:52:09 +0800

    Added Serialized Dictionary package
    Added in the Serialized Dictionary package by 'ayellowpaper' to replace the custom one I was working on.

--- Assets/Scripts/Light Switches/LightSwitch.cs
+++ Assets/Scripts/Light Switches/LightSwitch.cs
@@ -1,60 +1,18 @@
+using System;
 using System.Collections;
-using System.Collections.Generic;
-using System.Runtime.Serialization;
-using System.Security.Cryptography;
 using UnityEngine;
 
-public class LightSwitch : MonoBehaviour, ISerializationCallbackReceiver
+namespace AYellowpaper.SerializedCollections
 {
-    [SerializeField]
-    public GameObject lightSwitch = null;
-    public bool modifyValues; //A check for when the lists that store the dictionary data can be cleared
-
-    [SerializeField]
-    private DictionaryScriptableObject dictionaryData; //The data that will be shown in the inspector
-    [SerializeField]
-    private List<GameObject> lightMesh = new List<GameObject>(); //Dictionary Key
-    [SerializeField]
-    private List<Light> lights = new List<Light>(); //Dictionary value
-    private Dictionary<GameObject, Light> lightModels = new Dictionary<GameObject, Light>();
-
-    public void OnAfterDeserialize()
+    public class LightSwitch : MonoBehaviour
     {
-        if(!modifyValues) //Checks if the lists that store the dictionary data can be modified
+        [SerializedDictionary("Light Model", "Point Light")]
+        public SerializedDictionary<GameObject, Light> lightModels;
+        private GameObject switchObject;
+
+        public void Awake()
         {
-            lightMesh.Clear(); //Clears the current light meshes
-            lights.Clear(); //Clears the current lights
-
-            for (int i = 0; i < Mathf.Min(dictionaryData.LightMesh.Count, dictionaryData.Lights.Count); i++) //Loops through all elements in the dictionary data object and adds them
-            {
-                lightMesh.Add(dictionaryData.LightMesh[i]);
-                lights.Add(dictionaryData.Lights[i]);
-            }
+            switchObject = this.gameObject;
         }
-    }
-
-    public void OnBeforeSerialize()
-    {
-        
-    }
-
-    public void DeserializeDictionary()
-    {
-        Debug.Log("NOTICE: Deserializing dictionary data for dictionary data object " + dictionaryData.name);
-
-        lightModels = new Dictionary<GameObject, Light>();
-
-        dictionaryData.LightMesh.Clear(); //Clears the current light meshes
-        dictionaryData.Lights.Clear(); //Clears the current lights
-
-        for (int i = 0; i < Mathf.Min(lightMesh.Count, lights.Count); i++) //Loops through all elements in the lists and adds them to the dictionary (Based on smallest list size)
-        {
-            lightMesh.Add(dictionaryData.LightMesh[i]);
-            lights.Add(dictionaryData.Lights[i]);
-
-            lightModels.Add(lightMesh[i], lights[i]);
-        }
-
-        modifyValues = false;
     }
 }
