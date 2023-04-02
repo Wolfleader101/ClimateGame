02016f1242b14e6696710838696cc0404a4ac9e0
Darcy Wilson <26544407+VoxAndrews@users.noreply.github.com>
2023-04-03 00:52:48 +0800

    Added Dictionary Objects + Light Switch Awake Method
    Added Dictionary Objects which contain a pair of light objects and meshes which the user can set + The Light Switch method checks and sets up the lights in the dictionary based on if they are set to on or off with the lightOnOrOff boolean

--- Assets/ScriptableObjects/DictionaryData.cs
+++ Assets/ScriptableObjects/DictionaryData.cs
@@ -6,5 +6,5 @@ using UnityEngine;
 [CreateAssetMenu(fileName = "New Dictionary Data Object", menuName = "Data Container/Dictionary Data Object")]
 public class DictionaryData : SerializedScriptableObject
 {
-    public Dictionary<GameObject, Light>[] lightModels; //Holds a pair, one gameobject for one light
+    public Dictionary<GameObject, Light> lightModels; //Holds a pair, one gameobject for one light
 }
--- Assets/Scripts/Light Switches/LightSwitch.cs
+++ Assets/Scripts/Light Switches/LightSwitch.cs
@@ -14,9 +14,32 @@ public class LightSwitch : MonoBehaviour
     private GameObject switchObject;
 
     private const float maxLightIntensity = 0.3f;
+    private const float maxEmissiveIntensity = 5.0f;
+    private Color emissiveColor = new Color(0.654f, 0.749f, 0.450f);
 
     public void Awake()
     {
         switchObject = this.gameObject;
+
+        if (lightOnOrOff == false)
+        {
+            foreach (KeyValuePair<GameObject, Light> item in dictionaryData.lightModels) //Loops through all elements within the data dictionary
+            {
+                item.Key.GetComponent<Renderer>().material.DisableKeyword("_EMISSION"); //Turns off the emission
+
+                item.Value.intensity = 0.0f;
+            }
+        }
+        else
+        {
+            foreach (KeyValuePair<GameObject, Light> item in dictionaryData.lightModels) //Loops through all elements within the data dictionary
+            {
+                item.Key.GetComponent<Renderer>().material.EnableKeyword("_EMISSION"); //Turns on the emission
+
+                item.Key.GetComponent<Renderer>().material.SetColor("_EmissiveColor", emissiveColor * maxEmissiveIntensity); //Sets the colour and intensity
+
+                item.Value.intensity = maxLightIntensity;
+            }
+        }
     }
 }
