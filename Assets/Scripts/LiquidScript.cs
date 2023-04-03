a990d0c63fc48d8402e44ce0bb4e7ebdb2fb2f46
Caleb <cjmeepers@gmail.com>
2023-04-03 16:14:37 +0800

    move and remove shit

--- Assets/Scripts/LiquidScript.cs
+++ Assets/Scripts/LiquidScript.cs
@@ -23,12 +23,14 @@ public class LiquidScript : MonoBehaviour
     private float wave;
     private float time = 0.5f;
 
-    private Vector3 pos;
+    private Vector3 lastPos, pos;
+    private Quaternion lastRot;
+
+    private Vector3 angularVelocity, velocity;
 
     private Vector3 comp;
 
     private Material mat;
-    private Rigidbody rb;
     private Mesh mesh;
 
     private void Awake()
@@ -47,13 +49,6 @@ public class LiquidScript : MonoBehaviour
             mesh = GetComponent<MeshFilter>().sharedMesh;
         if (mat == null)
             mat = GetComponent<MeshRenderer>().sharedMaterial;
-        if (rb == null)
-        {
-            rb = GetComponentInParent<Rigidbody>();
-
-            if (rb == null)
-                rb = GetComponent<Rigidbody>();
-        }
     }
 
     private void Update()
@@ -66,19 +61,25 @@ public class LiquidScript : MonoBehaviour
             wobbleAddZ = Mathf.Lerp(wobbleAddZ, 0.0f, Time.deltaTime * recovery);
 
             pulse = 2 * Mathf.PI * wobbleSpeed;
-            wave = Mathf.Lerp(wave, Mathf.Sin(pulse * time), Time.deltaTime * Mathf.Clamp(rb.velocity.sqrMagnitude + rb.angularVelocity.sqrMagnitude, thickness, 10.0f));
+            wave = Mathf.Lerp(wave, Mathf.Sin(pulse * time), Time.deltaTime * Mathf.Clamp(velocity.magnitude + angularVelocity.magnitude, thickness, 10.0f));
 
             wobbleX = wobbleAddX * wave;
             wobbleZ = wobbleAddZ * wave;
 
-            wobbleAddX += Mathf.Clamp((rb.velocity.x + (rb.velocity.y * 0.2f) + rb.angularVelocity.z + rb.angularVelocity.y) * maxWobble, -maxWobble, maxWobble);
-            wobbleAddZ += Mathf.Clamp((rb.velocity.z + (rb.velocity.y * 0.2f) + rb.angularVelocity.x + rb.angularVelocity.y) * maxWobble, -maxWobble, maxWobble);
+            velocity = (lastPos - transform.position) / Time.deltaTime;
+            angularVelocity = GetAngularVelocity(lastRot, transform.rotation);
+
+            wobbleAddX += Mathf.Clamp((velocity.x + (velocity.y * 0.2f) + angularVelocity.z + angularVelocity.y) * maxWobble, -maxWobble, maxWobble);
+            wobbleAddZ += Mathf.Clamp((velocity.z + (velocity.y * 0.2f) + angularVelocity.x + angularVelocity.y) * maxWobble, -maxWobble, maxWobble);
         }
 
         mat.SetFloat("_WobbleX", wobbleX);
         mat.SetFloat("_WobbleZ", wobbleZ);
 
         UpdatePosition();
+
+        lastPos = transform.position;
+        lastRot = transform.rotation;
     }
 
     private void UpdatePosition()
@@ -98,6 +99,35 @@ public class LiquidScript : MonoBehaviour
             pos = new Vector3(0.0f, Mathf.Lerp(-1.5f, 0.5f, fillAmount), 0.0f);
 
         mat.SetVector("_FillAmount", pos);
+    }
+
+    Vector3 GetAngularVelocity(Quaternion foreLastFrameRotation, Quaternion lastFrameRotation)
+    {
+        var q = lastFrameRotation * Quaternion.Inverse(foreLastFrameRotation);
+        // no rotation?
+        // You may want to increase this closer to 1 if you want to handle very small rotations.
+        // Beware, if it is too close to one your answer will be Nan
+        if (Mathf.Abs(q.w) > 1023.5f / 1024.0f)
+            return Vector3.zero;
+        float gain;
+        // handle negatives, we could just flip it but this is faster
+        if (q.w < 0.0f)
+        {
+            var angle = Mathf.Acos(-q.w);
+            gain = -2.0f * angle / (Mathf.Sin(angle) * Time.deltaTime);
+        }
+        else
+        {
+            var angle = Mathf.Acos(q.w);
+            gain = 2.0f * angle / (Mathf.Sin(angle) * Time.deltaTime);
+        }
+        Vector3 angularVelocity = new Vector3(q.x * gain, q.y * gain, q.z * gain);
+ 
+        if (float.IsNaN(angularVelocity.z))
+        {
+            angularVelocity = Vector3.zero;
+        }
+        return angularVelocity;
     }
 
     private float GetLowestPointOnMesh()
