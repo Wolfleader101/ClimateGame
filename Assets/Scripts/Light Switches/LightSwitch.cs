97fb1cc9d7a4524b2658b674e5323361c4754edb
Darcy Wilson <26544407+VoxAndrews@users.noreply.github.com>
2023-04-03 15:45:38 +0800

    Raycasting & Outline Working
    Raycasting works to identify the object with the LightSwitch tag and then turn on the outline for it

--- Assets/Scripts/Light Switches/LightSwitch.cs
+++ Assets/Scripts/Light Switches/LightSwitch.cs
@@ -3,27 +3,32 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.XR;
 
 public class LightSwitch : MonoBehaviour
 {
     //PUBLIC VARIABLES
-    public DictionaryData dictionaryData;
+    public DictionaryData lightData; //The dictionary data which holds the light and light model pair (Key is the model, value is the light)
+
     public bool lightOnOrOff = true; //A boolean to turn lights on or off
 
     //PRIVATE VARIABLES
-    private GameObject switchObject;
+    private const float maxLightIntensity = 0.3f; //The maximum light intensity value
+    private const float maxEmissiveIntensity = 5.0f; //The maximum emission intensity value
+    private const float maximumRayLength = 0.5f; //The maximum length of the ray
 
-    private const float maxLightIntensity = 0.3f;
-    private const float maxEmissiveIntensity = 5.0f;
-    private Color emissiveColor = new Color(0.654f, 0.749f, 0.450f);
+    private Color emissiveColour = new Color(0.654f, 0.749f, 0.450f); //The emissive colour
+    private Color outlineColour = Color.yellow;
+
+    private bool xrModeOn; //A boolean to check if the game is using VR or not
+
+    private RaycastHit hitData; //A variable to hold the data from a raycast hit
 
     public void Awake()
     {
-        switchObject = this.gameObject;
-
         if (lightOnOrOff == false)
         {
-            foreach (KeyValuePair<GameObject, Light> item in dictionaryData.lightModels) //Loops through all elements within the data dictionary
+            foreach (KeyValuePair<GameObject, Light> item in lightData.lightModels) //Loops through all elements within the data dictionary
             {
                 item.Key.GetComponent<Renderer>().material.DisableKeyword("_EMISSION"); //Turns off the emission
 
@@ -32,14 +37,59 @@ public class LightSwitch : MonoBehaviour
         }
         else
         {
-            foreach (KeyValuePair<GameObject, Light> item in dictionaryData.lightModels) //Loops through all elements within the data dictionary
+            foreach (KeyValuePair<GameObject, Light> item in lightData.lightModels) //Loops through all elements within the data dictionary
             {
                 item.Key.GetComponent<Renderer>().material.EnableKeyword("_EMISSION"); //Turns on the emission
 
-                item.Key.GetComponent<Renderer>().material.SetColor("_EmissiveColor", emissiveColor * maxEmissiveIntensity); //Sets the colour and intensity
+                item.Key.GetComponent<Renderer>().material.SetColor("_EmissiveColor", emissiveColour * maxEmissiveIntensity); //Sets the colour and intensity
 
                 item.Value.intensity = maxLightIntensity;
             }
         }
+
+        if(XRSettings.enabled) //If VR is enabled
+        {
+            xrModeOn = true;
+        }
+        else
+        {
+            xrModeOn = false;
+        }
+    }
+
+    public void Update()
+    {
+        if (xrModeOn) //Checks to see if VR mode is on
+        {
+
+        }
+        else
+        {
+            RayToSwitch(Camera.main.ViewportPointToRay(new Vector3(0.5f, 0.5f, 0)));
+        }
+    }
+
+    private bool RayToSwitch(Ray inputRay) //A function for checking if the ray is hitting the 
+    {
+        Ray rayToPoint = inputRay; //Updates the current ray to the new input ray
+
+        Debug.DrawRay(Camera.main.transform.position, Camera.main.transform.forward); //Draws a ray in the scene mode to check that it is casting
+
+        if (Physics.Raycast(rayToPoint, out hitData, maximumRayLength) && hitData.transform.gameObject.tag == "LightSwitch")
+        {
+            Debug.Log("NOTICE: Light Switch being hit is '" + hitData.transform.gameObject.name + "'");
+
+            gameObject.GetComponent<Outline>().enabled = true;
+
+            gameObject.GetComponent<Outline>().OutlineMode = Outline.Mode.OutlineVisible;
+            gameObject.GetComponent<Outline>().OutlineColor = Color.yellow;
+            gameObject.GetComponent<Outline>().OutlineWidth = 10.0f;
+
+            return true;
+        }
+        else
+            gameObject.GetComponent<Outline>().enabled = false;
+
+        return false;
     }
 }
