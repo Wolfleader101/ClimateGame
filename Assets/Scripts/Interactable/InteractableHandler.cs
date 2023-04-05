3bdaa7963c41e7ca3e03632e8c9b26dd490f6860
Wolfleader <james.morgan@iinet.net.au>
2023-04-05 09:41:49 +0800

    basic VR interactions

--- Assets/Scripts/Interactable/InteractableHandler.cs
+++ Assets/Scripts/Interactable/InteractableHandler.cs
@@ -1,18 +1,35 @@
-﻿using UnityEngine;
+﻿using System;
+using UnityEngine;
 using UnityEngine.InputSystem;
+using UnityEngine.XR;
 
 [RequireComponent(typeof(PlayerInput))]
 public class InteractableHandler : MonoBehaviour
 {
+    [SerializeField] private GameObject vrLeftHand;
+    [SerializeField] private GameObject vrRightHand;
+
     private Camera _camera;
 
     private Interactable _heldInteractable;
 
+    private bool _vrEnabled;
+
     private void Start()
     {
         _camera = Camera.main;
+        _vrEnabled = XRSettings.enabled;
+
+        vrLeftHand.SetActive(_vrEnabled);
+        vrRightHand.SetActive(_vrEnabled);
     }
 
+    public void OnVRInteract(Interactable interactable)
+    {
+        if (interactable.Holdable) _heldInteractable = interactable;
+        interactable.OnInteract(this);
+    }
+    
     public void OnInteract(InputAction.CallbackContext context)
     {
         if (!context.performed) return;
@@ -33,7 +50,6 @@ public class InteractableHandler : MonoBehaviour
 
             if (interactable.Holdable) _heldInteractable = interactable;
             interactable.OnInteract(this);
-
         }
     }
 }
---
+++ Assets/Scripts/Interactable/VRHandInteraction.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+
+public class VRHandInteraction : MonoBehaviour
+{
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (!collision.gameObject.GetComponent<Interactable>()) return;
+
+        transform.parent.GetComponent<InteractableHandler>().OnVRInteract(collision.gameObject.GetComponent<Interactable>());
+    }
+}
