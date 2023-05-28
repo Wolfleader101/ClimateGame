7e646fa8b16b88443a9c63678e9c2ac2272a78ad
Caleb <cjmeepers@gmail.com>
2023-05-28 22:37:12 +0800

    Update PhoneScript.cs

--- Assets/Scripts/Custom/UI/PhoneScript.cs
+++ Assets/Scripts/Custom/UI/PhoneScript.cs
@@ -12,6 +12,9 @@ public class PhoneScript : MonoBehaviour
 
     [SerializeField] private Vector3 eulerRotation;
 
+    [SerializeField] private float minZ = -160.0f;
+    [SerializeField] private float maxZ = -70.0f;
+
     private void Start()
     {
         foreach (Transform child in transform)
@@ -27,6 +30,26 @@ public class PhoneScript : MonoBehaviour
     {
         eulerRotation = LH_controller.localEulerAngles;
 
-        print(eulerRotation);
+        if (eulerRotation.z >= minZ && eulerRotation.z <= maxZ)
+        {
+            SetColor(false);
+            print("good rotation");
+        }
+        else
+        {
+            SetColor(true);
+
+        }
+    }
+
+    private void SetColor(bool clear)
+    {
+        for (int i = 0; i < images.Count; ++i)
+        {
+            if (clear)
+                images[i].color = Color.Lerp(Color.white, Color.clear, Time.deltaTime);
+            else
+                images[i].color = Color.Lerp(Color.clear, Color.white, Time.deltaTime);
+        }
     }
 }
