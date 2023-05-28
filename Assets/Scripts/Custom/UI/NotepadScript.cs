76abb145da67f8de1d69c3eed384a6cb9f4b1c14
Caleb <cjmeepers@gmail.com>
2023-05-28 19:23:49 +0800

    test

--- Assets/Scripts/Custom/UI/NotepadScript.cs
+++ Assets/Scripts/Custom/UI/NotepadScript.cs
@@ -6,12 +6,16 @@ public class NotepadScript : MonoBehaviour
     private Animator animator;
     private bool opened = false;
 
+    private AudioSource source;
+    private AudioClip clip;
+
     [SerializeField] private TMP_Text task_1_t;
     [SerializeField] private TMP_Text task_2_t;
 
     private void Awake()
     {
         animator = GetComponent<Animator>();
+        source = GetComponent<AudioSource>();
     }
 
     private void Start()
@@ -26,7 +30,15 @@ public class NotepadScript : MonoBehaviour
         else if (Input.GetKeyUp(KeyCode.Tab))
             animator.SetBool("opened", opened = false);
 
-        if (Input.GetKeyDown(KeyCode.T))
+        if (Input.GetKeyDown(KeyCode.T) && opened)
+        {
             task_1_t.text = "<s>EXAMPLE TASK 1:</s>";
+            OnStrikethrough();
+        }
+    }
+
+    private void OnStrikethrough()
+    {
+        source.Play();
     }
 }
