2742b6b7fc4402770a5a610ae37ebe2599227e84
Caleb <cjmeepers@gmail.com>
2023-05-28 22:59:02 +0800

    ch

--- Assets/Scripts/Custom/UI/PhoneScript.cs
+++ Assets/Scripts/Custom/UI/PhoneScript.cs
@@ -1,55 +1,26 @@
 using UnityEngine;
-using UnityEngine.UI;
-using UnityEngine.InputSystem;
-
-using System.Collections.Generic;
 
 public class PhoneScript : MonoBehaviour
 {
     [SerializeField] private Transform LH_controller;
-
-    [SerializeField] private List<Image> images;
-
     [SerializeField] private Vector3 eulerRotation;
 
-    [SerializeField] private float minZ = -160.0f;
-    [SerializeField] private float maxZ = -70.0f;
+    [SerializeField] private Canvas canvas;
+
+    [SerializeField] private float minZ = 0.0f;
+    [SerializeField] private float maxZ = 0.0f;
 
     private void Start()
     {
-        foreach (Transform child in transform)
-        {
-            if (child.TryGetComponent<Image>(out Image image))
-            {
-                images.Add(image);
-            }
-        }
     }
 
     private void Update()
     {
         eulerRotation = LH_controller.localEulerAngles;
 
-        if (eulerRotation.z >= minZ && eulerRotation.z <= maxZ)
-        {
-            SetColor(false);
-            print("good rotation");
-        }
+        if (eulerRotation.z <= minZ && eulerRotation.z >= maxZ)
+            canvas.gameObject.SetActive(true);
         else
-        {
-            SetColor(true);
-
-        }
-    }
-
-    private void SetColor(bool clear)
-    {
-        for (int i = 0; i < images.Count; ++i)
-        {
-            if (clear)
-                images[i].color = Color.Lerp(Color.white, Color.clear, Time.deltaTime);
-            else
-                images[i].color = Color.Lerp(Color.clear, Color.white, Time.deltaTime);
-        }
+            canvas.gameObject.SetActive(false);
     }
 }
